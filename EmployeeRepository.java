4740c677c8e4d5745c4354e507f3c7f190676a71
Harshal Gangrade <harshalgangrade01@gmail.com>
2021-08-25 11:22:00 +0530

    Add files via upload

---
+++ EmployeeRepository.java
@@ -0,0 +1,13 @@
+package com.book.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.book.entity.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
+	
+}
